20e9a3550844d1a0b2c94bc5aea0d2deb42d5687
AlexRider <xandermuhia2@gmail.com>
2022-09-08 01:59:30 -0700

    Task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return -Display message to terminal
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+
+}
