3c4a3103ee370c5ab48739bb406eca6e89200caa
AlexRider <xandermuhia2@gmail.com>
2022-09-08 02:28:13 -0700

    Task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include<stdio>
+
+
+/**
+ * main -entry to the program
+ *
+ * Return:prints the  message
+ * "with proper grammar, but the outcome is a piece of art"
+ * to the screen
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
