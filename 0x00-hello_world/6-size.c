ab5b30df2cd1dca4eafab72bac4ab734bd6c3827
AlexRider <xandermuhia2@gmail.com>
2022-09-08 03:15:38 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/**the terminal depending on whethere the system is   32-bit
+/**
+ * the terminal depending on whethere the system is   32-bit
  * or a 64-bit system
  * Return: 0 (sucess)
 */
