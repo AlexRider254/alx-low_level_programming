fb32fe944db3d12a4268399f495427614a0c556b
AlexRider <xandermuhia2@gmail.com>
2022-09-09 04:04:13 -0700

    KDB

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry of the program
+ * Description: The program prints out all the possible combination of
+ * single digit
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return 0;
+}
