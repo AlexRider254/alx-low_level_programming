85f35a51b0f8f85387c15edff5b3269d3e0f947d
AlexRider <xandermuhia2@gmail.com>
2022-09-09 02:58:41 -0700

    Task5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the program
+ * Description: This program prints out all the alophabet 
+ * and then prints out a newline
+ * Return: 0 (Successfull)
+ */
+int main(void)
+{
+
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if ((letter == 'e') || (letter == 'q'))
+		{
+			letter++;
+		}
+		else
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	
+
+	putchar('\n');
+
+	return (0);
+}
