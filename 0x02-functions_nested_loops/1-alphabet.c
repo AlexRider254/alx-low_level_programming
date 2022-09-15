ef8ea5c4b628c927b1e4971f511313e8fee16045
AlexRider <xandermuhia2@gmail.com>
2022-09-14 22:45:44 -0700

    Task2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Introduction to the program
+ * 
+ * Description: Function that prints abc
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
