85097a4223c77f417d29798db5f5418c48a0351e
AlexRider <xandermuhia2@gmail.com>
2022-09-15 00:19:28 -0700

    Task6

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,21 +10,21 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	int hours_remainder;
-	int min_reminder;
+	int min_remainder;
 
 	while (hours <= 23)
 	{
 		while (minutes <=59)
 		{
-			min_remainder = minutes % 10;`
+			min_remainder = minutes % 10;
 			hours_remainder = hours % 10;
-			_putchar(hours / 10 + =);
-			_putchar(hours_remainder + 0);
-			_putchar( );
-			_putchar(minutes / 10 + 0);
-			_putchar(min_remainder + 0);
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remainder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(min_remainder + '0');
 			minutes++;
-			_putchar(   );
+			_putchar('\n');
 		}
 		hours++
 		minutes = 0; 
